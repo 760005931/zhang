eb238588a273f747b4ceee9f8c1e6c6a85c4ac99
zhang <760005931@qq.com>
2024-09-03 18:04:34 +0800

    git提交

--- src/main.js
+++ src/main.js
@@ -5,12 +5,6 @@ import App from './App.vue'
 import router from './router'
 //引入初始样式
 import '@/styles/common.scss'
-
-//测试接口函数
-import {getCategory} from '@/apis/testAPI'
-getCategory().then(res => {
-    console.log(res);
-})
 const app = createApp(App)
 
 app.use(createPinia())
