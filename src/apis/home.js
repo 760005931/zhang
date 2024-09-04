11a9d64391e69e4feb72480532b981899cb1c532
zhang <760005931@qq.com>
2024-09-04 16:54:21 +0800

    home-新鲜好物和人气推荐实现

--- src/apis/home.js
+++ src/apis/home.js
@@ -1,17 +1,23 @@
-import httpInstance from '@/utils/http'
-
-//封装banner
-/**
- * 获取首页轮播图的API请求
- * 
- * 本函数通过调用HTTP实例发送GET请求来获取首页的轮播图数据
- * 轮播图通常位于网页的顶部，用于展示重要信息或推荐内容
- * 
- * @return {Promise} 返回一个Promise对象，包含请求到的首页轮播图数据
- */
-export function getBannerAPI() {
-    // 调用HTTP实例发送GET请求，传入轮播图的API路径
+import httpInstance from "@/utils/http";
+//获取banner
+export function getBannerAPI(){
     return httpInstance({
-        url: '/home/banner'
-    });
+        url:'/home/banner'
+    })
+}
+//获取新鲜好物
+export function getNewAPI() {
+    return httpInstance({
+        url: '/home/new'
+    })
+}
+/**
+ * @description: 获取人气推荐
+ * @param {*}
+ * @return {*}
+ */
+export const getHotAPI = () => {
+    return httpInstance({
+        url:'/home/hot'
+    })
 }
