e0f7d4839db503c2476487420ca25154148f0f1b
zhang <760005931@qq.com>
2024-09-04 14:53:03 +0800

    pinia优化重复请求-layout布局

--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -1,14 +1,7 @@
 <script setup>
-import {getCategoryAPI} from '@/apis/layout'
-import { onMounted,ref } from 'vue';
-const categoryList = ref([])
-const getCategory = async() => {
-    const res =  await getCategoryAPI()
-    categoryList.value = res.result
-}
-onMounted(() => {
-     getCategory()
-})
+// 使用pinia中的事件
+import { useCategoryStore } from '@/stores/category';
+const categoryStore = useCategoryStore()
 </script> 
 
 <template>
@@ -21,7 +14,7 @@ onMounted(() => {
                 <li class="home">
                     <RouterLink to="/">首页</RouterLink>
                 </li>
-                <li v-for="item in categoryList" :key="item.id">
+                <li v-for="item in categoryStore.categoryList" :key="item.id">
                     <RouterLink to="/">{{ item.name }}</RouterLink>
                 </li>
             </ul>
